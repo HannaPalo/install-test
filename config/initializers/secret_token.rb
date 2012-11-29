5bf9a226fdf6f6e81e404a80d6a1c70f085fb270
Hanna Palo <hanna.palo@tbwa.fi>
2012-11-29 10:26:46 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HannapaloDemo::Application.config.secret_token = 'a228bd04bdbce88128c6279f56b97214bc77511ad7a7f7894830d7aabc1635ad654761f12e8b151b2f6e0c99b3ceafe5aaca0d5fc492790f668f36b8553aa073'
